571dcc3837e463cd1c6b001982dfee9f5659b06c
Mahid970 <mmahidujjaman@gmail.com>
2024-10-25 22:06:27 +0600

    completed routing

---
+++ src/components/applied-jobs/AppliedJobs.jsx
@@ -0,0 +1,5 @@
+const AppliedJobs = () => {
+  return <div>h1 this is Applied Jobs</div>;
+};
+
+export default AppliedJobs;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,38 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/root/Root.jsx";
+import Home from "./components/home/Home.jsx";
+import Statistics from "./components/statistics/Statistics.jsx";
+import AppliedJobs from "./components/applied-jobs/AppliedJobs.jsx";
+import Blog from "./components/blog/Blog.jsx";
+import ErrorPage from "./components/error-page/ErrorPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/statistics",
+        element: <Statistics></Statistics>,
+      },
+      {
+        path: "/applied-jobs",
+        element: <AppliedJobs></AppliedJobs>,
+      },
+      {
+        path: "/blogs",
+        element: <Blog></Blog>,
+      },
+    ],
   },
 ]);
 
